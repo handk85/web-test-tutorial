8ca2bd9eaa01de5d606eba3eaf994ed897c2706b
DongGyun Han <DongGyun.Han@rhul.ac.uk>
2023-02-04 14:58:33 +0000

    move koa app to separate file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,37 +1,4 @@
-import Koa from "koa";
-import Router from "koa-router";
-import json from "koa-json";
-import logger from "koa-logger";
-import cors from "@koa/cors";
-import bodyParser from "koa-body";
-
-import { listItems, getItem, addItem } from "./database";
-
-const app = new Koa();
-const router = new Router();
-
-function addRoute(path, handler) {
-  router.get(path, async (ctx, next) => {
-    ctx.body = await handler(ctx);
-    await next();
-  });
-}
-
-addRoute("/list", listItems);
-addRoute("/item", getItem);
-
-router.post("/add", async (ctx, next) => {
-  ctx.body = await addItem(ctx);
-  await next();
-});
-
-app
-  .use(json())
-  .use(logger())
-  .use(cors())
-  .use(bodyParser())
-  .use(router.routes())
-  .use(router.allowedMethods());
+import app from "./app";
 
 app.listen(3001, () => {
   console.log("Listening port 3001");
